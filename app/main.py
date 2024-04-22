641e4b5e7f7a16e51e5e2304ac3cab0bac88fcda
Edgar Alan Rodriguez Amador <edgaralanra@gmail.com>
2024-04-21 23:41:18 -0600

    fix: cors rules

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.title = "Example deploy on railway"
 app.version = "0.0.1"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/', tags=['home'])
 def home():
